f8e4a404cc0f61204de8a3dad2f8ed5599c8ed67
Christopher Zenner <christopher.zenner@gmail.com>
2020-06-28 09:06:55 -0700

    feat: scrape news site and parse data

--- controllers/newsscraper-controller.js
+++ controllers/newsscraper-controller.js
@@ -1,5 +1,7 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
 const router = require('express').Router();
-// const db = require('../models');
+const db = require('../models');
 
 module.exports = (() => {
   router.get('/', (req, res) => {
@@ -7,6 +9,34 @@ module.exports = (() => {
     console.log(`And this is your response: ${res}`);
 
     res.render('index');
+  });
+
+  router.get('/scrape', (req, res) => {
+    axios.get('https://www.democracynow.org/').then((response) => {
+      const $ = cheerio.load(response.data);
+      const results = [];
+
+      $('.news_item').each(function (i, elem) {
+        // DEBUG:
+        // console.log(`Article ${i} = ${$(this).contents()}`);
+
+        const result = {};
+        const link = $(this).find('h3').children('a');
+
+        result.title = link.text();
+        result.link = link.attr('href');
+        result.date = $(this).find('.date').first().text();
+        result.img = $(this).find('.play').attr('src');
+
+        // DEBUG:
+        // console.log(`Article ${i} = ${JSON.stringify(result)}`);
+
+        // DEBUG:
+        results.push(result);
+      });
+
+      res.json(results);
+    });
   });
 
   return router;
