b0e4a38361527b5c31ae9b67a1ee671ab15225f9
Christopher Zenner <christopher.zenner@gmail.com>
2020-06-28 15:58:19 -0700

    test: determine why count query isn't returning number
    The query `db.Article.find().count();` is returning an Object, rather than a number.
    
    Task
    - [ ] Determine why this is.

--- controllers/newsscraper-controller.js
+++ controllers/newsscraper-controller.js
@@ -1,17 +1,40 @@
+// Node dependencies
 const axios = require('axios');
 const cheerio = require('cheerio');
+const mongoose = require('mongoose');
+// const time = require('moment');
 const router = require('express').Router();
+
+// Local resources
+const source = 'https://www.democracynow.org/';
 const db = require('../models');
+
+// Mongo Database
+mongoose.connect(
+  'mongodb://localhost/newsscraperdb',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
 
 module.exports = (() => {
   router.get('/', (req, res) => {
-    const hbsObj = { msg: 'Hello, World!' };
+    // const hbsObj = { msg: 'Hello, World!' };
+    //
+    // res.render('index', hbsObj);
 
-    res.render('index', hbsObj);
+    db.Article.find((err, docs) => {
+      if (err) throw (err);
+
+      // DEBUG:
+      console.log(docs);
+
+      res.render('index', { headlines: docs });
+    }).sort({ timestamp: -1 }).limit(10);
   });
 
   router.get('/scrape', (req, res) => {
-    axios.get('https://www.democracynow.org/').then((response) => {
+    axios.get(source).then((response) => {
       const $ = cheerio.load(response.data);
       const results = [];
 
@@ -23,16 +46,37 @@ module.exports = (() => {
         const link = $(this).find('h3').children('a');
 
         result.title = link.text();
-        result.link = link.attr('href');
+        result.link =
+          `${source.slice(0, source.length - 1)}${link.attr('href')}`;
         result.date = $(this).find('.date').first().text();
-        result.img = $(this).find('.play').attr('src');
+        result.image =
+          $(this).find('picture').children('img').attr('src');
 
         // DEBUG:
-        // console.log(`Article ${i} = ${JSON.stringify(result)}`);
+        console.log(`Article ${i} = ${JSON.stringify(result)}`);
 
-        // DEBUG:
-        results.push(result);
+        if (!db.Article.find({ title: result.title }).count()) {
+          db.Article.create(result)
+            .then((dbArticle) => {
+              console.log(dbArticle);
+            })
+            .catch(console.err);
+
+          // DEBUG:
+          results.push(result);
+        }
       });
+
+      /*
+        Test Code
+
+        Query is returning Object, rather than number. Why?
+      */
+      const test = db.Article.find().count();
+
+      console.log('count: ' + JSON.stringify(test));
+
+      // <-- End Test Code -->
 
       res.json(results);
     });
